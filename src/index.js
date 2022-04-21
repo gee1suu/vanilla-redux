021a251385f911f2fae81e83ee594228713c294b
gee1suu <therese86126@ewhain.net>
2022-04-21 19:32:34 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return ++count;
+  } else if (action.type === "MINUS") {
+    return --count;
+  } else {
+    return count;
+  }
 };
 
 const countStore = configureStore({
   reducer: countModifier,
 });
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
