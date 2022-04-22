dc43d2311f7e29dc2f489fb7e6f3e64506b3781f
gee1suu <therese86126@ewhain.net>
2022-04-22 19:18:16 +0900

    #3.3 mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { actionCreators } from "../store";
 
 function Home() {
   const toDos = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -10,6 +13,8 @@ function Home() {
 
   function onSubmit(e) {
     e.preventDefault();
+    console.log(text);
+    dispatch(actionCreators.addToDo(text));
     setText("");
   }
 
--- src/store.js
+++ src/store.js
@@ -3,14 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
-export const addToDo = (text) => {
+const addToDo = (text) => {
   return { type: ADD_TODO, text };
 };
-export const deleteToDo = (id) => {
+const deleteToDo = (id) => {
   return { type: DELETE_TODO, id };
 };
 
-const reducer = (state = [""], action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       const newToDoObj = { text: action.text, id: Date.now() };
@@ -26,5 +26,10 @@ const reducer = (state = [""], action) => {
 const store = configureStore({
   reducer: reducer,
 });
+
+export const actionCreators = {
+  addToDo,
+  deleteToDo,
+};
 
 export default store;
